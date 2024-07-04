d7d942f4ffd7698c7cf431bf58d70490b33f9f34
Farruh Zoirov <fzoirov29@gmail.com>
2024-07-04 10:44:01 +0500

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hi there, We deployed successfully to heroku! ');
+});
+
+// Server is running here
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
